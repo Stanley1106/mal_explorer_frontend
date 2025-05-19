856f00d167f658958dbd788d81f51618753375b2
Stanley <Stanley1106chen@gmail.com>
2025-05-20 01:08:14 +0800

    Add login page png

--- components/login-form.tsx
+++ components/login-form.tsx
@@ -88,8 +88,7 @@ export function LoginForm({
           </form>
           <div className="bg-muted relative hidden md:block">
             <img
-              src="/placeholder.svg"
-              alt="Image"
+              src="/placeholder.png"
               className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
             />
           </div>
