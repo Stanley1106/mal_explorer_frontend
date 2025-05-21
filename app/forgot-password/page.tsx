d9f69a26135067898d2b02e3a298d36a5ba0dd25
Stanley <Stanley1106chen@gmail.com>
2025-05-21 19:42:19 +0800

    style: 調整忘記密碼頁面的排版樣式

--- app/forgot-password/page.tsx
+++ app/forgot-password/page.tsx
@@ -2,8 +2,10 @@ import { ForgetPasswordForm } from "@/components/forget-password-form"
 
 export default function ForgetPasswordPage() {
   return (
-    <div className="container relative flex min-h-screen flex-col items-center justify-center">
-      <ForgetPasswordForm />
+    <div className="bg-muted flex min-h-svh flex-col items-center justify-center p-6 md:p-10">
+      <div className="w-full max-w-sm md:max-w-xl">
+        <ForgetPasswordForm />
+      </div>
     </div>
   )
 }
